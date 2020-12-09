008f0f42b3a66401839aff9b182b9434cade8516
Ura Chen 陳鈺青 <1901518@C0698.local>
2020-12-09 15:18:34 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 npm run build &&
 git checkout -b gh-pages &&
 rm -rf src tests css js img public *.json *.ico *.js *.css *.html *.png *.jpg *.gif *.jpeg *.lock *.sh *.md &&
-mv dist/* ./ &&
-rm -rf dist;
+mv src/dist/* ./ &&
+rm -rf src/dist;
 git add . &&
 git commit . -m "update" &&
 git push --set-upstream origin gh-pages &&
