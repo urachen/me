ccf1865a0ce0885f9dcf554b9eda8c7ae3f3a51c
Ura Chen 陳鈺青 <1901518@C0698.local>
2020-12-24 09:57:33 +0800

    add scrollbehavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,17 @@
 import VueRouter from 'vue-router';
 import routes from './routes';
-
-// Vue.use(VueRouter);
-
-// const base = _.get(window, '__INIT_STATE__.state.system.base', '/');
-
 const router = new VueRouter({
   routes,
   mode: 'hash',
   base: '/me',
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 });
 
-router.beforeEach((to, from, next) => {
-  // if (to.name === 'Login' && localStorage.getItem('token')) {
-  //   store.dispatch('getUserProfile').then(() => {
-  //     return next(store.state.userProfile.routerLandingPage)
-  //   })
 
-  // }
-  // if (to.meta.guarded && !localStorage.getItem('token')) {
-  //   return next({ name: 'Login' });
-  // }
+
+router.beforeEach((to, from, next) => {
   next();
 });
 
